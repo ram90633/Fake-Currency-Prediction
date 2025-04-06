d68efcb744cd25de7de63d28900ee686528cf7d0
Abhiram <yarlagaddaabhi5@gmail.com>
2025-04-06 11:29:46 +0530

    Add files via upload

---
+++ Dataset Path.py
@@ -0,0 +1,6 @@
+#importinf dataset from kagglehub
+
+import kagglehub
+jayaprakashpondy_indian_currency_dataset_path = kagglehub.dataset_download('jayaprakashpondy/indian-currency-dataset')
+
+print('Data source import complete.')
